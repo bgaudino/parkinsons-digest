2932ee862dfdb0a5a6379f33cd9fef33cc3a2925
bgaudino <bgaudino@fusionbox.com>
2026-06-09 10:15:30 -0500

    Update production settings

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -12,3 +12,9 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = (
+    "HTTP_X_FORWARDED_PROTO",
+    "https",
+)
